fd956e0c0e89d16595233283a931102687a33e35
Dipen Chauhan <get.dipen@gmail.com>
2016-12-30 11:39:40 -0800

    Added validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,34 @@
+# == Schema Information
+#
+# Table name: addresses
+#
+#  id               :integer          not null, primary key
+#  address1         :string
+#  address2         :string
+#  city             :string
+#  state            :string
+#  zip              :string
+#  country          :string
+#  phone            :string
+#  fax              :string
+#  email            :string
+#  addressable_type :string
+#  addressable_id   :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 class Address < ApplicationRecord
-  belongs_to :addressable, polymorphic: true
+	belongs_to :addressable, polymorphic: true
+	# Normalizes the attribute itself before validation
+	phony_normalize :phone, default_country_code: 'US'
+	phony_normalize :fax, default_country_code: 'US'
+	
+	validates :address1, presence: true
+	validates :city, presence: true
+	validates :state, presence: true
+	validates :zip, presence: true
+	validates :country, presence: true
+	validates :phone, presence: true, phony_plausible: true
+
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: properties
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  access_code :string
+#  notes       :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Property < ApplicationRecord
-	has_one :address, as: :addressable
+	has_one :address, as: :addressable, dependent: :destroy
+
+	validates :name, presence: true
+	validates :access_code, presence: true
 end
