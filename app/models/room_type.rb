c7556897716860b6ef6f2e28df95ee855bb2e6d6
Dipen Chauhan <get.dipen@gmail.com>
2017-01-10 11:06:19 -0800

    Added validations

--- app/models/room_type.rb
+++ app/models/room_type.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: room_types
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  code        :string(4)
+#  description :string
+#  property_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class RoomType < ApplicationRecord
   belongs_to :property
+  before_save :covert_code_to_upper_case
+
+  validates :name, presence: true
+  validates :code, presence: true, :length => { :maximum => 4 }
+  validates :description, presence: true
+
+  private
+  def convert_code_to_upper_case
+  	self.code.uppercase!  	
+  end
 end
