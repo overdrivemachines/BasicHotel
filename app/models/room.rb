e2b44056c4e93fcfb22daf255cc001beb1973c72
Dipen Chauhan <get.dipen@gmail.com>
2017-01-15 12:02:55 -0800

    Added initialization defaults

--- app/models/room.rb
+++ app/models/room.rb
@@ -15,9 +15,23 @@
 
 class Room < ApplicationRecord
   belongs_to :room_type
+  after_initialize :default_values
 
   validates :room_number, presence: true
   validates :vacant, presence: true 
   validates :clean, presence: true
   validates :inventory, presence: true
+
+  # TODO: Might remove this method
+  def default_values
+  	if (self.vacant.nil?)
+  		self.vacant = true
+  	end
+  	if (self.clean.nil?)
+  		self.clean = true
+  	end
+  	if (self.inventory.nil?)
+  		self.inventory = true
+  	end
+  end
 end
